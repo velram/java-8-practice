7dbb29b088d5b8f552be5258ee262a9e46b28963
Velmurugan Moorthy <velmurugan1993@gmail.com>
2020-01-08 12:25:52 +0530

    Vel | Java-8 | Lambdas practice | Lecture #315 - practice

---
+++ java-8-hands-on/src/com/practice/java8/lambdas/LambdaTester.java
@@ -0,0 +1,16 @@
+package com.practice.java8.lambdas;
+
+/**
+ * @author Velmurugan Moorthy
+ * 08.01.2020
+ * This class is to practice the Lambda basics
+ * Reference : Java course in Udemy by Dheeru Mundluru
+ * Lecture URL :
+ * https://www.udemy.com/course/java-in-depth-become-a-complete-java-engineer/learn/lecture/6242632#overview
+ */
+
+public class LambdaTester {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!! ");
+    }
+}
